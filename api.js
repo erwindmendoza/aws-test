4f01958d7cee6e36a41fb33d516a42f838fa22e2
erwindmendoza <91693525+erwindmendoza@users.noreply.github.com>
2022-10-05 09:59:01 +0800

    basic serverless deployment.

--- api.js
+++ api.js
@@ -1,38 +1,38 @@
-const db = require("./db");
-const {
-    GetitemCommand
-} = require("@aws-sdk/clinet-dynamodb");
-const { marshall, unmarshall } = require("@aws-sdk/util/dynamodb");
+// const db = require("./db");
+// const {
+//     GetitemCommand
+// } = require("@aws-sdk/clinet-dynamodb");
+// const { marshall, unmarshall } = require("@aws-sdk/util/dynamodb");
 
-const getPost = async (event) => {
-    const response = { statusCode: 200 };
+// const getPost = async (event) => {
+//     const response = { statusCode: 200 };
 
-    try {
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({ postId: event.pathParameters.postId }),
-        };
-        const { Item } = await db.send(new GetitemCommand(params));
+//     try {
+//         const params = {
+//             TableName: process.env.DYNAMODB_TABLE_NAME,
+//             Key: marshall({ postId: event.pathParameters.postId }),
+//         };
+//         const { Item } = await db.send(new GetitemCommand(params));
 
-        console.log({ Item });
-        response.body = JSON.stringify({
-            msg: "success",
-            data: (Item) ? unmarshall(Item) : {},
-            rawData: Item,
-        });
-    } catch (e) {
-        console.error(e);
-        response.statusCode = 500;
-        response.body = JSON.stringify({
-            msg: "failed",
-            errorMsg: e.message,
-            errorStack: e.stack,
-        });
-    }
+//         console.log({ Item });
+//         response.body = JSON.stringify({
+//             msg: "success",
+//             data: (Item) ? unmarshall(Item) : {},
+//             rawData: Item,
+//         });
+//     } catch (e) {
+//         console.error(e);
+//         response.statusCode = 500;
+//         response.body = JSON.stringify({
+//             msg: "failed",
+//             errorMsg: e.message,
+//             errorStack: e.stack,
+//         });
+//     }
 
-    return response;
-};
+//     return response;
+// };
 
-module.exports = {
-    getPost
-}
+// module.exports = {
+//     getPost
+// }
